e9a3132b2c4362f98fce381279e8d07ba396833c
Haris Barki <harisbarki@ymail.com>
2017-12-21 22:03:14 -0330

    moved to new http client for angular 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './components/';
@@ -16,7 +16,7 @@ import {AppRoutingModule, routingComponents} from './app.routes';
 	imports: [
 		BrowserModule,
 		FormsModule,
-		HttpModule,
+		HttpClientModule,
 		AppRoutingModule
 	],
 	providers: [
