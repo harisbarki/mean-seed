68bb5e22408f5c96ed5183615ba39f1a0ec5b60d
Haris Barki <harisbarki@ymail.com>
2018-06-18 11:24:11 -0230

    added promise then to seed

--- server/src/components/seed/seed.routes.js
+++ server/src/components/seed/seed.routes.js
@@ -10,26 +10,30 @@ const FILE_NAME = `seed.routes.js`;
 // Get by ID
 router.get('/', (req, res) => {
 	const id = req.query['_id'] ? req.query['_id'] : '';
-	let promise = SeedCtrl.get(id);
-	// genericFunctions.handlePromise(promise, res, 'get', MODEL_NAME, FILE_NAME);
+	let promise = SeedCtrl.get(id).then((data) => {
+		res.status(200).json(data);
+	});
 });
 
 // Create
 router.post('/', (req, res) => {
-	let promise = SeedCtrl.create(req.body);
-	// genericFunctions.handlePromise(promise, res, 'post', MODEL_NAME, FILE_NAME);
+	let promise = SeedCtrl.create(req.body).then((data) => {
+		res.status(200).json(data);
+	});
 });
 
 // Update
 router.patch('/', (req, res) => {
-	let promise = SeedCtrl.update(req.body);
-	// genericFunctions.handlePromise(promise, res, 'patch', MODEL_NAME, FILE_NAME);
+	let promise = SeedCtrl.update(req.body).then((data) => {
+		res.status(200).json(data);
+	});
 });
 
 // Delete by ID
 router.delete('/', (req, res) => {
-	let promise = SeedCtrl.remove(req.body['_id']);
-	// genericFunctions.handlePromise(promise, res, 'delete', MODEL_NAME, FILE_NAME);
+	let promise = SeedCtrl.remove(req.body['_id']).then((data) => {
+		res.status(200).json(data);
+	});
 });
 
 module.exports = router;
